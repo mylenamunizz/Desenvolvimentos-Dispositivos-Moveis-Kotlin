871af40d47e906f800f5ea2c9c69395513f53ea6
mylenamunizz <mylena.muniz04@gmail.com>
2025-09-22 11:18:52 -0300

    .

--- Trabalho Pratico 04 UI/TP_usuarios_cadastro/gradle/wrapper/gradle/app/src/main/java/com/example/myapplication/ItemData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication
-
-import com.example.myapplication.models.ItemLista
-
-object ItemData {
-    val itens = mutableListOf<ItemLista>();
-}
--- Trabalho Pratico 04 UI/TP_usuarios_cadastro/gradle/wrapper/gradle/app/src/main/java/com/example/myapplication/models/ItemLista.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.models
-
-data class ItemLista (
-    val nome: String,
-    val valor: Float,
-    val link: String
-)
